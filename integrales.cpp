34c071b163ff11033cc2e31892fb271d9c798697
cristofervert <105964721+cristofervert@users.noreply.github.com>
2024-11-29 15:31:26 -0800

    Correcciones

--- integrales.cpp
+++ integrales.cpp
@@ -10,7 +10,7 @@ double f(double x){
 }
 
 double sumaRiemann(double a, double b, double n=10){
-    double suma = 0, x = a;
+    double suma = 0, x;
     double deltax = (b-a)/n; // para saber el tamaño de la base
     
     for(int i=0; i<n; i++){
@@ -21,12 +21,12 @@ double sumaRiemann(double a, double b, double n=10){
 }
 
 double reglaDelTrapecio(double a, double b, double n=10){
-    double suma = 0, x = a;
+    double suma = 0, x;
     double resultado = 0.0;
     double deltax = (b-a)/n; // para saber el tamaño de la base    
     for(int i=0; i<n; i++){
-        x = a + i*deltax;
-        (i==0 || i==n) ? suma += f(x): suma += 2*f(x); // se suman puras alturas
+        x = a + i*deltax; // Error: i == n -> no se llega a ese valor en el bucle 
+        (i==0 || i==(n-1)) ? suma += f(x): suma += 2*f(x); // se suman puras alturas
     }
     resultado = (suma*deltax)*0.5;
     return resultado;
@@ -34,14 +34,14 @@ double reglaDelTrapecio(double a, double b, double n=10){
 
 double reglaDeSimpson(double a, double b, double n=10){
     int test = (int) n;
-    double suma = 0, x = a;
+    double suma = 0, x;
     double resultado = 0.0;
     double deltax = (b-a)/n; // para saber el tamaño de la base  
     
     //std::cout << test <<  "\t" << test%2 << "\n";
     for(int i=0; i<n; i++){
         x = a + i*deltax;
-        if( i==0 || i==n ){
+        if( i==0 || i==(n-1) ){ //Error: i == n -> no se llega a ese valor en el bucle
             suma += f(x);
         }
         else if(i%2){
@@ -51,15 +51,15 @@ double reglaDeSimpson(double a, double b, double n=10){
             suma += 2*f(x);
         }
     }
-    resultado = (suma*deltax)*(1/3);
+    resultado = (suma*deltax)*0.333333; // el uso de 1/3 resultaba en el redondeo a 0
     return resultado;
 }
 
 int main(){
     double a=0, b=1.6;
     std::cout << "Integracion numerica\n";
-    std::cout << "RDT -> 100 partes: " << reglaDelTrapecio(a,b,10) << "\n";
-    std::cout << "SR -> 100 partes: " << sumaRiemann(a,b, 10) << "\n";
-    std::cout << "RS -> 100 partes: " << reglaDeSimpson(a,b,10) << "\n";
+    std::cout << "RDT -> 100 partes: " << reglaDelTrapecio(a,b,100) << "\n";
+    std::cout << "SR -> 100 partes: " << sumaRiemann(a,b, 100) << "\n";
+    std::cout << "RS -> 100 partes: " << reglaDeSimpson(a,b,100) << "\n";
     return 0;
 }
